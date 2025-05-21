09bf17810a8bc2ebf5150ab2f69eec91faedca4b
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-05-21 13:26:54 +0300

    feat: enhance app window and branding

--- src/electron/main.js
+++ src/electron/main.js
@@ -3,8 +3,14 @@ import path from 'path'
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 768,
+    center: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
   mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'))
   // mainWindow.webContents.openDevTools();
